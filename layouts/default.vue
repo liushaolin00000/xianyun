fb7b080da94afa45700b9010691515477fccf94f
linlin000 <53568232+linlin000s@users.noreply.github.com>
2019-10-12 09:09:44 +0800

    基本环境配置和首页完成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
+    <!-- 页面头部占位组件 -->
+    <Header class="content"></Header>
+    <!-- 内容站位组件 -->
     <nuxt />
+    <!-- 底部组件的占位组件 -->
+    <Footer></Footer>
   </div>
 </template>
-
+<script>
+//导入头部组件
+import Header from  '@/components/header.vue' 
+//导入底部组件
+import Footer from '@/components/footer.vue'
+export default {
+    components:{
+      //注册头部组件
+      Header,
+      //注册底部组件
+      Footer
+    }
+}
+</script>
 <style>
 html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -17,13 +35,16 @@ html {
   box-sizing: border-box;
 }
 
-*,
+* {
+  margin: 0;
+  top: 0;
+}
+
 *:before,
 *:after {
   box-sizing: border-box;
   margin: 0;
 }
-
 .button--green {
   display: inline-block;
   border-radius: 4px;
@@ -51,5 +72,20 @@ html {
 .button--grey:hover {
   color: #fff;
   background-color: #35495e;
+}
+ul,
+li,
+ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+a:hover {
+}
+em,
+i {
+  font-style: normal;
 }
 </style>
---
+++ pages/tab.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="main">
+    <div class="btn">
+      <span @click="handleplay(index)" :class="current==index?'gaoliang':'none'" v-for='(item,index) in title' 
+      :key='index'
+      style="cursor:pointer">{{item.title}}</span>
+    </div>
+    <div v-if="current==0">
+      <p>娱乐1</p>
+      <p>娱乐2</p>
+      <p>娱乐3</p>
+      <p>娱乐4</p>
+    </div>
+    <div v-if="current==1">
+      <p>新闻1</p>
+      <p>新闻2</p>
+      <p>新闻3</p>
+      <p>新闻4</p>
+    </div>
+    <div v-if="current==2">
+      <p>游戏1</p>
+      <p>游戏2</p>
+      <p>游戏3</p>
+      <p>游戏4</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // 标题内容
+      title:[{title:'娱乐'},{title:'新闻'},{title:'游戏'}],
+      current: 0
+    };
+  },
+  methods: {
+    handleplay(index) {
+      this.current=index
+    }
+  }
+};
+</script>
+
+<style lang='less' scoped>
+.main {
+  .btn {
+    .gaoliang{
+    background-color: pink;
+    }
+    span {
+      border: 1px solid black;
+    }
+  }
+}
+</style>
